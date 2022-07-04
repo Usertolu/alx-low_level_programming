a9e5ed56f204569fd2f116af17b445df4934cbca
Usertolu <omotosotolu11@gmail.com>
2022-07-04 15:46:40 -0700

    alphabt

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main- Prints alphabet in lower case, followed by a new line
+ * Return: Always 0
+ */
+int main(void)
+{
+	char letter;
+	
+	for (letter = 'a'; letter <= 'z'; letter++)
+	{
+		if (letter != 'e' && letter != 'q')
+			putchar(letter);
+	}
+
+	putchar('\n');
+
+	return (0);
+}
