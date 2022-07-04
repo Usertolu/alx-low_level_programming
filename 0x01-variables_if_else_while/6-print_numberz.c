33889fd98128ade4a2977eb40b0d40c02fd3d61d
Usertolu <omotosotolu11@gmail.com>
2022-07-04 16:04:34 -0700

    numberz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main- Prints all single digits from zero to ten and add a new line
+ * Return: Always 0
+ */
+int main(void)
+{
+	for (num = 0; num < 10; num++)
+		putchar((num % 10) + '0');
+	putchar('\n');
+
+	return (0);
+}
