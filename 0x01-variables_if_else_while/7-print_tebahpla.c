e0081a48590da554d84543d03fb86ddb6051a18d
Usertolu <omotosotolu11@gmail.com>
2022-07-04 16:12:29 -0700

    tebah

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**main- prints lower case of alphabet in reverse
+ * Return: Always 0
+ */
+int main(void)
+{
+	char letter;
+
+	for (letter = 'z'; letter >= 'a'; letter-)
+		putchar(letter);
+
+	putchar('\n');
+
+	return (0);
+}
