28de9b8134745b8f56d2fd690460fa1bfc18073a
Usertolu <omotosotolu11@gmail.com>
2022-07-05 17:07:35 -0700

    comb4

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -12,7 +12,7 @@ int main(void)
 	for (digit1 = 0; digit1 < 8; digit1++)
 	{
 		for (digit2 = digit1 + 1; digit2 < 9; digit2++)
-                {
+		{
 			for (digit3 = digit2 + 1; digit3 < 10; digit3++)
 			{
 				putchar((digit1 % 10) + '0');
@@ -21,14 +21,14 @@ int main(void)
 
 				if (digit1 == 7 && digit2 == 8 && digit3 == 9)
 					continue;
-				
+
 				putchar(',');
 				putchar(' ');
 			}
 		}
 	}
-	
+
 	putchar('\n');
-	
+
 	return (0);
 }
